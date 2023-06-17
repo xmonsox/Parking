ff6175118a934a14d55f2ff4f1dc4050e08e00d5
Monsalve <juandavidmonsalve@gmail.com>
2023-06-16 20:32:46 -0500

    Versio con Modulos de interfaces Terminadas o de prueba

---
+++ Parqueadero/src/Clases/Database.java
@@ -0,0 +1,173 @@
+package Clases;
+
+import com.mysql.cj.protocol.Resultset;
+import java.sql.*;
+
+public class Database {
+    
+    Statement manipularDB;
+    
+    public Database(){
+        String hostname = "localhost";
+        String puerto = "3306";
+        String database_name = "parqueadero";
+        String database_user = "root";
+        String database_password = "";
+        String url = "jdbc:mysql://"+hostname+":"+puerto+"/"+database_name;
+        
+        try{
+            Connection conexion = DriverManager.getConnection(url, database_user, database_password);
+            this.manipularDB = conexion.createStatement();
+            System.out.println("Conexion Exitosa a: "+database_name);
+        }catch(SQLException e){
+            System.out.println("Error en conexion: "+e.getMessage());
+            this.manipularDB = null;
+        }
+    }
+    
+   /* public Persona[] listaClientes(){
+        Persona [] listaClientes = new Persona [100];
+        try{
+            ResultSet registros = this.manipularDB.executeQuery("SELECT * FROM clientes");
+            registros.next();
+            if (registros.getRow()==1) {
+                int indice = 0;
+                do{
+                    Persona temp = new Persona( registros.getString("cedula"),registros.getString("nombres"), registros.getString("apellidos"), registros.getString("telefono"), registros.getString("direccion"), registros.getString("email") );
+                    listaClientes[indice] = temp;
+                    indice++;
+                }while(registros.next());
+            }
+            return listaClientes;
+        }catch(SQLException e){
+            System.out.println("Error en SELECT: "+e.getMessage());
+            return listaClientes;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    public Persona buscarCliente(String cedula){
+        Persona temp = null;
+        try{
+            ResultSet registros = this.manipularDB.executeQuery("SELECT * FROM clientes WHERE cedula='"+cedula+"' ");
+            registros.next();
+            if (registros.getRow()==1) {
+                temp = new Persona( registros.getString("cedula"),registros.getString("nombres"), registros.getString("apellidos"), registros.getString("telefono"), registros.getString("direccion"), registros.getString("email") );
+            }
+            return temp;
+        }catch(SQLException e){
+            System.out.println("Error en SELECT: "+e.getMessage());
+            return temp;
+        }
+    }
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    public boolean editarCliente(Persona persona){
+        boolean respuesta = false;
+        String cedula = persona.getCedula();
+        String nombres = persona.getNombres();
+        String apellidos = persona.getApellidos();
+        String telefono = persona.getTelefono();
+        String direccion = persona.getDireccion();
+        String email = persona.getEmail();
+         
+        
+        try {
+            String consulta = "UPDATE clientes SET nombres='"+nombres+"', apellidos='"+apellidos+"', telefono='"+telefono+"', direccion='"+direccion+"', email='"+email+"' WHERE cedula='"+cedula+"'";
+            int resp = manipularDB.executeUpdate(consulta);
+            if (resp>0) {
+                respuesta = true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error en UPDATE: "+ex.getMessage());
+        }
+        return respuesta;
+    }
+    
+    
+    
+   
+    
+    public boolean insertarCliente(String cedula, String nombres, String apellidos, String telefono, String direccion, String email){
+        String consulta = "INSERT INTO clientes(cedula, nombres, apellidos, telefono, direccion, email) VALUES ('"+cedula+"','"+nombres+"','"+apellidos+"','"+telefono+"','"+direccion+"','"+email+"') ";    
+        try{
+            int respuesta = manipularDB.executeUpdate(consulta);
+            if (respuesta>0) {
+                System.out.println("REGISTRO INSERTADO CON EXITO");
+                return true;
+            }else{
+                return false;
+            }
+        }catch(SQLException e){
+            System.out.println("Error al insertar: "+e.getMessage());
+            return false;
+        }
+    }
+    
+    public boolean eliminarCliente(Persona persona){
+        boolean respuesta = false;
+        String cedula = persona.getCedula();
+        String nombres = persona.getNombres();
+        String apellidos = persona.getApellidos();
+        String telefono = persona.getTelefono();
+        String direccion = persona.getDireccion();
+        String email = persona.getEmail();
+
+        try {
+            String consulta = "DELETE FROM clientes WHERE cedula=' "+cedula+" ' ";
+            int resp_consulta = manipularDB.executeUpdate(consulta);
+            if (resp_consulta>0) {
+                respuesta = true;
+            }
+        }catch (SQLException ex) {
+            System.out.println("Error en UPDATE: "+ex.getMessage());
+        }
+        return respuesta;
+    }
+    
+    
+    
+    
+         
+    public boolean insertarProducto(Producto producto){
+        int id = producto.getId();
+        String nombre = producto.getNombre();
+        int precio = producto.getPrecio();
+
+
+        String consulta = "INSERT INTO productos(id, nombre, precio) VALUES (' "+id+" ', ' "+nombre+" ', ' "+precio+" ') ";    
+
+        try{
+            int respuesta = manipularDB.executeUpdate(consulta);
+            if (respuesta>0) {
+                System.out.println("PRODUCTO INSERTADO CON EXITO");
+                return true;
+            }else{
+                return false;
+            }
+        }catch(SQLException e){
+            System.out.println("Error al insertar: "+e.getMessage());
+            System.out.println("consulta:" + consulta);
+            return false;
+        }
+    }*/
+         
+         
+}
+    
+
